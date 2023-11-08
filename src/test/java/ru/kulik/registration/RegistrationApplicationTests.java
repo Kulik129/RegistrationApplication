7430a4bd13e38989eff675c28d5e64ff85ff26a0
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-11-08 17:38:35 +0300

    git init

---
+++ src/test/java/ru/kulik/registration/RegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kulik.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
