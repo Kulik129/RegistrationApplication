56fccdd1bff93f76d60260068ba3c37a8c359feb
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-11-08 17:45:16 +0300

    Ветка develop

--- src/main/java/ru/kulik/registration/RegistrationApplication.java
+++ src/main/java/ru/kulik/registration/RegistrationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RegistrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegistrationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationApplication.class, args);
+    }
 
 }
--- src/test/java/ru/kulik/registration/RegistrationApplicationTests.java
+++ src/test/java/ru/kulik/registration/RegistrationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RegistrationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
