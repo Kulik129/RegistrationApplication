c367b16cef5ba2220fc191b12828bedc23593b02
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-11-09 11:45:12 +0300

    Настройка CORS

---
+++ src/main/java/ru/kulik/registration/cors/CorsConfig.java
@@ -0,0 +1,33 @@
+package ru.kulik.registration.cors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${allowed.origins}")
+    private String allowedOrigins;
+
+    @Value("allowed.methods")
+    private String allowedMethods;
+
+    @Value("allowed.headers")
+    private String allowedHeaders;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins) // Разрешить доступ только с этого источника
+                        .allowedMethods(allowedMethods) // Разрешить все HTTP методы
+                        .allowedHeaders(allowedHeaders); // Разрешить все заголовки
+            }
+        };
+    }
+}
