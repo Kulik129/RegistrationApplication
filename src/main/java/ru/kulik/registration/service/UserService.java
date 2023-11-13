256b4ea660acdd59b3a9fee924f45f9546d201f6
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-11-13 20:18:05 +0300

    Добавление нового функционала

--- src/main/java/ru/kulik/registration/service/UserService.java
+++ src/main/java/ru/kulik/registration/service/UserService.java
@@ -3,6 +3,7 @@ package ru.kulik.registration.service;
 import ru.kulik.registration.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Интерфейс для сервиса управления пользователями.
@@ -19,9 +20,23 @@ public interface UserService {
      * Получает пользователя по идентификатору.
      *
      * @param id Идентификатор пользователя.
-     * @return Найденный пользователь или null, если не найден.
+     * @return Optional, содержащий пользователя, если найден, или пустой, если не найден.
      */
-    User getUser(long id);
+    Optional<User> getUserByID(long id);
+
+    /**
+     * Получает пользователя по email.
+     * @param email email пользователя.
+     * @return Optional, содержащий пользователя, если найден, или пустой, если не найден.
+     */
+    Optional<User> getUserByEmail(String email);
+
+    /**
+     * Получает пользователя по номеру телефона.
+     * @param phone номер тел. пользователя.
+     * @return Optional, содержащий пользователя, если найден, или пустой, если не найден.
+     */
+    Optional<User> getUserByPhone(String phone);
 
     /**
      * Удаляет пользователя по идентификатору.
