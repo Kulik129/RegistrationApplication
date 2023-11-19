18260b6749b35550583a222aa2c3eab4be220f66
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-11-19 16:34:38 +0300

    Тесты

--- src/test/java/ru/kulik/registration/service/UserServiceTest.java
+++ src/test/java/ru/kulik/registration/service/UserServiceTest.java
@@ -1,130 +1,108 @@
 package ru.kulik.registration.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+import ru.kulik.registration.DTO.UserDto;
+import ru.kulik.registration.exception.UserNotFoundException;
 import ru.kulik.registration.model.User;
+import ru.kulik.registration.model.UserRole;
+import ru.kulik.registration.repository.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private UserServiceImpl userServiceImpl;
-
-    @Autowired
+    @Mock
+    private UserRepository userRepository;
+    @Mock
     private PasswordEncoder passwordEncoder;
+    @Mock
+    private ModelMapper modelMapper;
+    @InjectMocks
+    private UserServiceImpl userService;
 
     @Test
-    void testSaveUser() {
-        User user = new User("John", "Smith", "23.12.1968", "smith.jo@gmail.com", "89255555555", "secret");
-        userService.save(user);
+    void testSaveUserWithValidPassword() {
+        UserDto userDto = new UserDto("John", "Doe", "john@example.com", "123456");
+        User user = new User("John", "Doe", "john@example.com", "123456");
 
-        Optional<User> savedUser = userService.getUserByID(user.getId());
+        when(modelMapper.map(userDto, User.class)).thenReturn(user);
+        when(userRepository.save(user)).thenReturn(user);
 
-        assertNotNull(savedUser);
-
-        assertEquals("John", savedUser.get().getFirstName());
-        assertEquals("Smith", savedUser.get().getLastName());
-        assertEquals("23.12.1968", savedUser.get().getDateOfBirth());
-        assertEquals("smith.jo@gmail.com", savedUser.get().getEmail());
-        assertEquals("89255555555", savedUser.get().getPhoneNumber());
-        assertTrue(passwordEncoder.matches("secret", user.getPassword()), savedUser.get().getPassword());
+        assertDoesNotThrow(() -> userService.save(userDto));
+        verify(userRepository, times(1)).save(user);
     }
 
     @Test
-    void testGetUser() {
-        User user = new User("Alice", "Festa", "12.12.2000", "fest@gmail.com", "89858888888", "mypassword");
-        userService.save(user);
+    void testGetUserById() {
+        long userId = 1L;
+        User user = new User("John", "Doe", "john@example.com", "123456");
 
-        Optional<User> savedUser = userService.getUserByID(user.getId());
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(modelMapper.map(user, UserDto.class)).thenReturn(new UserDto("John", "Doe", "john@example.com", "123456"));
 
-        assertNotNull(savedUser);
+        Optional<UserDto> result = userService.getUserByID(userId);
 
-        assertEquals("Alice", savedUser.get().getFirstName());
-        assertEquals("Festa", savedUser.get().getLastName());
-        assertEquals("12.12.2000", savedUser.get().getDateOfBirth());
-        assertEquals("fest@gmail.com", savedUser.get().getEmail());
-        assertEquals("89858888888", savedUser.get().getPhoneNumber());
-        assertTrue(passwordEncoder.matches("mypassword", user.getPassword()), savedUser.get().getPassword());
+        assertTrue(result.isPresent());
+        assertEquals("John", result.get().getFirstName());
+        assertEquals("Doe", result.get().getLastName());
+        assertEquals("john@example.com", result.get().getEmail());
+        assertEquals("123456", result.get().getPassword());
     }
 
     @Test
-    void testDeleteUser() {
-        User user = new User("Bob", "Marlie", "18.11.1967", "marlie@gmai.com", "89681111111", "123456");
-        userService.save(user);
+    void testGetUserByIdNotFound() {
+        long userId = 1L;
 
-        Optional<User> savedUser = userService.getUserByID(user.getId());
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
 
-        assertNotNull(savedUser);
-
-        userService.delete(savedUser.get().getId());
-
-        Optional<User> deletedUser = userService.getUserByID(savedUser.get().getId());
-
-        assertFalse(deletedUser.isPresent());
-    }
-
-    @Test
-    void testGetAllUsers() {
-        List<User> users = userService.getAllUsers();
-
-        assertNotNull(users);
-
-        System.out.println("Количество пользователей в БД: " + users.size());
-
-        assertEquals(users.size(), userService.getAllUsers().size());
-        assertFalse(users.isEmpty());
+        assertThrows(UserNotFoundException.class, () -> userService.getUserByID(userId));
     }
 
     @Test
     void testGetByEmail() {
+        String email = "poll@gmai.com";
         User user = new User("Polli", "Shelby", "28.10.1967", "poll@gmai.com", "89681111111", "123456");
-        userService.save(user);
 
-        Optional<User> savedUser = userService.getUserByEmail(user.getEmail());
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        when(modelMapper.map(user, UserDto.class)).thenReturn(new UserDto("Polli", "Shelby", "28.10.1967", "poll@gmai.com", "89681111111", "123456"));
 
-        assertEquals("poll@gmai.com", savedUser.get().getEmail());
+        Optional<UserDto> result = userService.getUserByEmail(email);
+
+        assertTrue(result.isPresent());
+        assertEquals("Polli", result.get().getFirstName());
+        assertEquals("Shelby", result.get().getLastName());
+        assertEquals("poll@gmai.com", result.get().getEmail());
+        assertEquals("123456", result.get().getPassword());
     }
 
     @Test
     void testGetUserByPhone() {
+        String phone = "89685678945";
         User user = new User("Grey", "Shelby", "18.10.1967", "grey@gmai.com", "89685678945", "123456");
 
-        userService.save(user);
+        when(userRepository.findByPhoneNumber(phone)).thenReturn(Optional.of(user));
+        when(modelMapper.map(user, UserDto.class)).thenReturn(new UserDto("Grey", "Shelby", "18.10.1967", "grey@gmai.com", "89685678945", "123456"));
 
-        Optional<User> savedUser = userService.getUserByPhone(user.getPhoneNumber());
+        Optional<UserDto> result = userService.getUserByPhone(phone);
 
-        assertEquals("89685678945", savedUser.get().getPhoneNumber());
-    }
-
-    @Test
-    void testExistsByEmail() {
-        User user = new User("Ada", "Shelby", "12.03.1977", "ada@gmai.com", "89689874534", "123456");
-
-        userService.save(user);
-        boolean savedUser = userServiceImpl.existsByEmail(user.getEmail());
-
-        assertTrue(savedUser);
-    }
-
-    @Test
-    void testExistsByPhoneNumber() {
-        User user = new User("Ada", "Shelby", "12.03.1977", "ada@gmai.com", "89689874534", "123456");
-
-        userService.save(user);
-        boolean savedUser = userServiceImpl.existsByPhoneNumber(user.getPhoneNumber());
-
-        assertTrue(savedUser);
+        assertTrue(result.isPresent());
+        assertEquals("Grey", result.get().getFirstName());
+        assertEquals("Shelby", result.get().getLastName());
+        assertEquals("grey@gmai.com", result.get().getEmail());
+        assertEquals("123456", result.get().getPassword());
     }
 }
